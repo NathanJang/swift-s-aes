eb89401130e150a6bce72d5529afb1eaf99639eb
Jonathan Chan <jonchanyc@outlook.com>
2015-11-16 18:12:49 +0800

    Initial Commit

---
+++ swift-s-aes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-s-aes
+//
+//  Created by Jonathan Chan on 2015-11-16.
+//  Copyright © 2015 Jonathan Chan. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
