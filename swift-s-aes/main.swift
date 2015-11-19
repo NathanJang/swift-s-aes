5bb2201fe566379a162e9627d4bf0736d4eb473b
Jonathan Chan <jonchanyc@outlook.com>
2015-11-19 15:21:04 +0800

    Sequential printing

--- swift-s-aes/main.swift
+++ swift-s-aes/main.swift
@@ -9,11 +9,13 @@
 import Foundation
 
 let plainText = NibbleArray(nibbles: [0b0100, 0b1001, 0b0100, 0b0010]) // "IB" in ASCII
+print("Plain text: \(plainText)")
+
 let key = NibbleArray(nibbles: [0b0110, 0b1000, 0b0110, 0b1100]) // "hl" in ASCII
 let cipherText = plainText.encrypt(key: key) // Should be 1001 0101 0111 0101
-let decrypted = cipherText.decrypt(key: key) // Should be equal to plainText
-
-print("Plain text: \(plainText)")
 print("Cipher text: \(cipherText)")
+
+let decrypted = cipherText.decrypt(key: key) // Should be equal to plainText
 print("Decrypted: \(decrypted)")
+
 print("Decrypted is equal to plain text?: \(plainText == decrypted)") // Should be true
